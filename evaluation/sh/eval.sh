8b9e253bccf27347ee2aa574bbaa3784d731da26
Omri <omri.sapir@workday.com>
2025-04-20 15:29:40 +0300

    all

--- evaluation/sh/eval.sh
+++ evaluation/sh/eval.sh
@@ -8,7 +8,7 @@ SPLIT="test"
 NUM_TEST_SAMPLE=-1
 
 # English open datasets
-DATA_NAME="gsm8k,math,svamp,asdiv,mawps,carp_en,tabmwp,minerva_math,gaokao2023en,olympiadbench,college_math"
+DATA_NAME="gsm8k"
 TOKENIZERS_PARALLELISM=false \
 python3 -u math_eval.py \
     --model_name_or_path ${MODEL_NAME_OR_PATH} \
