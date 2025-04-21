7b8b5386b04ec792e9c01abcced22bff636dbc54
Omri <omri.sapir@workday.com>
2025-04-21 21:29:59 +0300

    all

--- evaluation/math_eval.py
+++ evaluation/math_eval.py
@@ -114,6 +114,7 @@ def setup(args):
             tensor_parallel_size=len(available_gpus) // args.pipeline_parallel_size,
             pipeline_parallel_size=args.pipeline_parallel_size,
             trust_remote_code=True,
+            dtype=torch.bfloat16,
         )
         tokenizer = None
         if args.apply_chat_template:
